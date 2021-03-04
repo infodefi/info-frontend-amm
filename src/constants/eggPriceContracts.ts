7fe00acf1b1f7516d22005828936971bad75c0ac
infodefi <79815221+infodefi@users.noreply.github.com>
2021-03-05 00:03:00 +0100

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x5B13a55a3B88B45CC3AB79841825D3393080a305 ',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
